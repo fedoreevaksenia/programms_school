be681bf4b82dffa017a3aa749ae70fc756b79d1d
Ксения <108325113+fedoreevaksenia@users.noreply.github.com>
2024-12-28 23:47:00 +0500

    Create 7.py

---
+++ Tasks2/7.py
@@ -0,0 +1,9 @@
+def count(st):
+    alphabet = "abcdefghiklmnopqrstuvwxyz"
+    sum = 0
+    for letter in st:
+        for i in range (len(alphabet)):
+            if letter == alphabet[i]:
+                sum += i+1
+    return sum
+print(count("aeg"))
